ff567d612f7bc83cc1450d50712027577c435cd0
Kate Kiatsiri <keidsiri@gmail.com>
2022-03-04 09:36:01 -0800

    install react-navigation/native

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello world This is a good start for Capstone project!! This will be fun</Text>
       <StatusBar style="auto" />
     </View>
   );
